7ad4abd963549e8040a3496f003d80943ba085b0
katarina <a.katarina.freire@gmail.com>
2026-07-15 18:22:41 +0000

    transição

---
+++ splash.js
@@ -0,0 +1,33 @@
+/* ==========================================================
+   SPLASH SCREEN
+========================================================== */
+
+const splash = document.querySelector(".inicio");
+
+/* Tempo da animação da logo */
+
+const tempoLogo = 3000;
+
+/* Tempo de pausa */
+
+const pausa = 500;
+
+/* Tempo do fade */
+
+const fade = 700;
+
+/* Quando terminar */
+
+setTimeout(() => {
+
+    splash.classList.add("fade-out");
+
+}, tempoLogo + pausa);
+
+/* Ir para Home */
+
+setTimeout(() => {
+
+    window.location.href = "./html/onboarding-I.html";
+
+}, tempoLogo + pausa + fade);
